0060fa4c9f93820bc27c4b3077e73d26a816eff2
Parker Bolick <pbolick@insiten.com>
2022-05-04 12:14:09 -0400

    new test

--- ios/RNStripeTerminal.m
+++ ios/RNStripeTerminal.m
@@ -234,10 +234,11 @@ RCT_EXPORT_METHOD(connectReader:(NSString *)serialNumber location:(NSString *)lo
              @"currency": intent.currency,
              @"metadata": intent.metadata
              };
-}
+}fdf
 
 RCT_EXPORT_METHOD(readReusableCard) {
-SCPReadReusableCardParameters *params = [SCPReadReusableCardParameters new];
+
+   SCPReadReusableCardParameters *params = [[SCPReadReusableCardParameters alloc] init];
    readCard = [SCPTerminal.shared readReusableCard:params completion:^(SCPPaymentMethod * _Nullable intent_, NSError * _Nullable error) {
         readCard = nil;
         if (error) {
